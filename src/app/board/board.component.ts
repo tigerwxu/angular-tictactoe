0be0e9cdcba080b02aebee27ad37ca2d77b5d526
Wenrui Xu <tigerwxu@gmail.com>
2021-03-17 20:15:14 +1300

    Add TS logic to board component

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -6,10 +6,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./board.component.scss']
 })
 export class BoardComponent implements OnInit {
+  
+  squares: any[];
+  xIsNext: boolean;
+  winner: string;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.newGame();
   }
+
+  newGame() {
+    this.squares = new Array(9).fill(null);
+    this.winner = null;
+    this.xIsNext = true;
+  }
+
+  makeMove(i: number) {
+    // Check if square has been clicked, insert piece if not
+    if (!this.squares[i]) {
+      this.squares[i] = this.player;
+      this.xIsNext = !this.xIsNext;
+    }
+  }
+
+  /**
+   * Check if there are three same letters in a line anywhere
+   * @returns The winner's letter, or null if there is no winner
+   */
+  calculateWinner() {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        this.squares[a] &&
+        this.squares[a] === this.squares[b] &&
+        this.squares[a] === this.squares[c]
+      ) {
+        return this.squares[a];
+      }
+    }
+    return null;
+  }
+
+  get player() {
+    return this.xIsNext ? 'X' : 'O';
+  }
+
 
 }
